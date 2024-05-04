a02cca9b6934d8fd1ad78cdb3879d0368c5eecd7
tthcanthus <168840723+tthcanthus@users.noreply.github.com>
2024-05-04 22:26:36 +0800

    Update and rename isdm.R to isdm and isem.R

--- isdm and isem.R
+++ isdm and isem.R
@@ -27,7 +27,7 @@ estimate.ISDM <- function(size,mat,rho1,rho2,v)
     
     rho.h0 <- begin.p$rho
     beta.h0 <- begin.p$coefficients[-1]
-    
+    v.n <- matrix(seq(1,15,length=15),1,15)
     
     if(v==0)
       nu.h <- 15
@@ -75,6 +75,7 @@ estimate.ISDM <- function(size,mat,rho1,rho2,v)
     
     rho.hr0 <- begin.pr$rho
     beta.hr0 <- begin.pr$coefficients[-1]
+    v.n <- matrix(seq(1,15,length=15),1,15)
     
     if(v==0)
      nu.h <- 15
@@ -108,4 +109,103 @@ estimate.ISDM <- function(size,mat,rho1,rho2,v)
         }
 }
 
-
+##################################----EM algorithm for ISEM----########################################
+estimate.ISEM <- function(size,mat,rho1,rho2,v)
+# size is the sample size
+# mat is the weight matrix  w
+# rho1 is the spatial parameter rho_1 of the center model
+# rho2 is the spatial parameter rho_2 of the range model
+# when v=0, the error follows normal distribution; when v=2, the error follows t-distribution t(2)
+{
+ 
+    I_n <- diag(rep(1,size))
+    data.fc = data.frame(y_c,x_c,x_r) 
+    X <- cbind(x_c,x_r)
+    formula <- y_c~.
+    srk12w <- mat2listw(mat)
+    
+    begin.p <- errorsarlm(formula,data.fc,srk12w)
+    rho.h <- begin.p$lambda
+    beta.h <- begin.p$coefficients[-1]
+    inter.h <- begin.p$coefficients[1]
+    s2.h <- begin.p$s2
+    nu.h <- 3
+    v.n <- matrix(seq(1,15,length=15),1,15)
+    
+    if(v==0)
+      nu.h <- 15
+    abs <- 1
+    while(abs>10^(-6)){
+      ##############-------E step--------###################
+      epsilon <- (I_n-rho.h*mat)%*%(y_c-X%*%beta.h-inter.h)
+      delta <- (epsilon)^2/s2.h
+      logu <- digamma(0.5*(nu.h+1))-log(0.5*(nu.h+delta))
+      u <- as.vector((nu.h+1)/(nu.h+delta))
+      x.u <- diag(sqrt(u))%*%X
+      yc.u <- diag(sqrt(u))%*%y_c
+      data.fcu <- data.frame(yc.u,x.u)
+      formula.c <- yc.u~.
+      rho.b <- rho.h
+      beta.b <- beta.h
+      s2.b <- s2.h
+      #############--------M step--------##################
+      log.gamma <- function(nu){
+        f <- sum(0.5*nu*logu-0.5*nu*u)+size*(-log(gamma(0.5*nu))+0.5*nu*log(0.5*nu))
+        f
+      }
+      v.loglike <- apply(v.n,2,log.gamma)
+      v.index <- which(v.loglike==max(v.loglike))
+      nu.h <- v.n[v.index]
+      a <- errorsarlm(formula.c,data.fcu,srk12w)
+      rho.h <- a$lambda
+      beta.h <- a$coefficients[-1]
+      s2.h <- a$s2
+      inter.h <- a$coefficients[1]
+      abs <- max(c(abs(rho.h-rho.b),abs(beta.h-beta.b),abs(s2.h-s2.b)))
+    }
+    result1 <- c(rho.h,beta.h,nu.h)
+    
+    
+    data.fr = data.frame(y_r,x_c,x_r) 
+    formula <- y_r~.
+    X <- cbind(x_c,x_r)
+    begin.pr <- errorsarlm(formula,data.fr,srk12w)
+    rho.hr <- begin.pr$lambda
+    beta.hr <- begin.pr$coefficients[-1]
+    inter.hr <- begin.pr$coefficients[1]
+    s2.hr <- begin.pr$s2
+    nu.h <- 3
+    v.n <- matrix(seq(1,15,length=15),1,15)
+    
+    if(v==0)
+      nu.h <- 15
+    abs <- 1
+    while(abs>10^(-6)){
+      ##############-------E step--------###################
+      epsilon <-  (I_n-rho.hr*mat)%*%(y_r-X%*%beta.hr-inter.hr)
+      delta <- (epsilon)^2/s2.hr
+      logu <- digamma(0.5*(nu.h+1))-log(0.5*(nu.h+delta))
+      u <- as.vector((nu.h+1)/(nu.h+delta))
+      x.u <- diag(sqrt(u))%*%X
+      yr.u <- diag(sqrt(u))%*%y_r
+      data.fru <- data.frame(yr.u,x.u)
+      formula <- yr.u~.
+      rho.b <- rho.hr
+      beta.b <- beta.hr
+      s2.b <- s2.hr
+      #############--------M step--------##################
+      log.gamma <- function(nu){
+        f <- sum(0.5*nu*logu-0.5*nu*u)+size*(-log(gamma(0.5*nu))+0.5*nu*log(0.5*nu))
+        f
+      }
+      v.loglike <- apply(v.n,2,log.gamma)
+      v.index <- which(v.loglike==max(v.loglike))
+      nu.h <- v.n[v.index]
+      a <- errorsarlm(formula,data.fru,srk12w,u)
+      rho.hr <- a$lambda
+      beta.hr <- a$coefficients[-1]
+      inter.hr <- a$coefficients[1]
+      s2.hr <- a$s2
+      abs <- max(c(abs(rho.hr-rho.b),abs(beta.hr-beta.b),abs(s2.hr-s2.b)))
+    }
+}
